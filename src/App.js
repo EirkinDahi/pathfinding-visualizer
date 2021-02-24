82df0f17dbaac4f1971b1341977fbb9e29d4f7bd
EirkinDahi <eirkin12@gmail.com>
2021-02-23 22:19:23 -0500

    Added PathfidingVisualizer component to the React app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import  PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer"
 
 function App() {
   return (
     <div className="App">
-      
+      <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
 }
